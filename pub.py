874dc13548db6c2782cc9746c9969ba92bc2e1f7
hicke <henric.helbro@gmail.com>
2018-01-31 21:31:23 +0100

    backup

--- pub.py
+++ pub.py
@@ -2,7 +2,9 @@ import redis
 import time
 
 queue = redis.StrictRedis(host='localhost', port=32770, db=4)
+r = redis.StrictRedis(host='localhost', port=32770, db=4)
 channel = queue.pubsub()
 
 for i in range(10):
-    queue.publish("test", i)
+    queue.publish("test", "This is message %s" % str(i))
+    r.set('Number', i, 15)
