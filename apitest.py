ddc1ebc9f7719b2bf324dae540bfb5f2cbf5c1f5
Henric <henric.helbro@gmail.com>
2018-02-02 09:33:22 +0100

    New information

--- apitest.py
+++ apitest.py
@@ -1,6 +1,7 @@
 import requests
 import redis
-import hashlib
+import uuid
+import json
 # import timeit
 
 """
@@ -9,14 +10,26 @@ into local Redis server. Hash added as guid. 10s ttl.
 """
 
 r = redis.StrictRedis(host='localhost', port=32768, db=6)
-s = hashlib.sha256()
 
-for i in range(10):
-    h = hashlib.sha256(str(i))
+d = {}
+d.update({})
+for i in range(5):
+    h = uuid.uuid4().hex
     # hex = h.hexdigest()
-    r.set('number: ' + str(i), requests.get("https://mnh4ufvgcd.execute-api.eu-west-1.amazonaws.com/testing/html").text + "\n" + h.hexdigest(), 10)
+    #r.set('number: ' + str(i), requests.get("https://mnh4ufvgcd.execute-api.eu-west-1.amazonaws.com/testing/html").text + "\n" + h.hexdigest())
+    d.update({"hash: " + str(i): h})
+    j = json.dumps(d)
+r.set("payload", j)
 
-
+# Konvertera payload till json
+# Implementera Cognito och Oauth2 mot API GW
 # calculate runtime
 # DynamoDB sync
 # POST to API
+
+r = {'is_claimed': 'True', 'rating': 3.5}
+r = json.dumps(r)
+loaded_r = json.loads(r)
+loaded_r['rating'] #Output 3.5
+type(r) #Output str
+type(loaded_r) #Output dict
