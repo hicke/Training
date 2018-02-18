8189ec599ba299a9f2e99b47fdb3c7e1bd566e45
Henric <henric.helbro@gmail.com>
2018-02-18 23:06:20 +0100

    new files again

--- httpcodes.py
+++ httpcodes.py
@@ -5,24 +5,31 @@ import redis
 r = redis.StrictRedis(host='localhost', port=32768, db=3)
 url = 'https://httpstatuses.com/'
 sauce = requests.get(url)
-soup = bs.BeautifulSoup(sauce.text,'lxml')
-#print(soup.prettify)
+soup = bs.BeautifulSoup(sauce.text, 'lxml')
+# print(soup.prettify)
 
 all_lists = soup.find_all('li')
 i = []
 
 for li in all_lists:
     i.append(li.text)
-           
+
     for num in i:
         error = num.split()
         error_code = error[0]
         error_text = ' '.join(map(str, error[1:]))
-        #r.set(error_code, error_text, 100)
-        #print(url + error_code)
+        # r.set(error_code, error_text, 100)
+        # print(url + error_code)
 
 
 full_url = url + error_code
 f = requests.get(full_url)
-print(f.text)
-                       
+# print(f.text)
+
+
+def get_full_text():
+
+    new_sauce = requests.get(full_url)
+    new_soup = bs.BeautifulSoup(new_sauce.text, 'lxml')
+    for paragraph in new_soup:
+        print(new_soup.p)
--- newyorkbestsellers.py
+++ newyorkbestsellers.py
@@ -1,5 +1,6 @@
-import bs4
+import bs4 as bs
 import requests
 
-book_url = requests.get("https://www.nytimes.com/books/best-sellers/").text
-print(book_url)
+book_url = requests.get("https://www.nytimes.com/books/best-sellers/")
+sauce = bs.BeautifulSoup(book_url.text, 'lxml')
+print(sauce)
