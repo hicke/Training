56244c842fec5471c0ef59307e10e6ffe1fda407
Henric <henric.helbro@gmail.com>
2018-02-26 09:34:10 +0100

    again

--- newyorkbestsellers.py
+++ newyorkbestsellers.py
@@ -1,6 +1,11 @@
 import bs4 as bs
 import requests
 
+
+# TODO: Fix the bestsellers based on category
+# FIXME: Nothing to fix
+
+
 book_url = requests.get("https://www.nytimes.com/books/best-sellers/")
 sauce = bs.BeautifulSoup(book_url.text, 'lxml')
 print(sauce)
